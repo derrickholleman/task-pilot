1f7c4a5ec1587d7379cdd057e20b62b003428491
Derrick Holleman <d.holleman@fetchrewards.com>
2025-10-24 17:04:33 -0500

    project cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
 import { TodoInputComponent } from './components/todo-input/todo-input.component'
 import { TodoListComponent } from './components/todo-list/todo-list.component'
 import { TodoStorageService, Todo } from './services/todo-storage.service'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoInputComponent, TodoListComponent],
+  imports: [TodoInputComponent, TodoListComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
